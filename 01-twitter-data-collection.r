0ea83601a396e07a984976a38ca2f0d98a529a7a
Pablo Barberá <pablo.barbera@nyu.edu>
2015-01-31 18:43:54 -0500

    added twitter examples

--- 01-twitter-data-collection.r
+++ 01-twitter-data-collection.r
@@ -10,10 +10,8 @@ setwd("~/Dropbox/git/social-media-workshop")
 ## INSTALLING PACKAGES THAT WE WILL USE TODAY
 doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
 toInstall <- c("ROAuth", "twitteR", "streamR", "ggplot2", 
-	"tm", "RCurl", "maps", "Snowball")
+	"tm", "RCurl", "maps", "Snowball", "Rfacebook")
 if(doInstall){install.packages(toInstall, repos = "http://cran.r-project.org")}
-if(doInstall){install.packages("Rstem", 
-	repos = "http://www.omegahat.org/R", type="source")}
 
 
 #####################################
@@ -51,6 +49,134 @@ searchTwitter('obama', n=1)
 
 ## now you can save oauth token for use in future sessions with twitteR or streamR
 save(my_oauth, file="backup/oauth_token.Rdata")
+
+#####################################
+### COLLECTING USER INFORMATION   ###
+#####################################
+
+library(twitteR)
+
+# profile information
+user <- getUser('barackobama')
+user$toDataFrame()
+
+# followers
+user$getFollowers(n=10)
+# (10 most recent followers)
+
+# friends (who they follow)
+user$getFriends(n=10)
+
+# see also smappR package (https://github.com/SMAPPNYU/smappR) for additional
+# functions to download users' data for a large number of users
+
+#####################################
+### SEARCH RECENT TWEETS		  ###
+#####################################
+
+# basic searches by keywords
+tweets <- searchTwitter("obama", n=20)
+# convert to data frame
+tweets <- twListToDF(tweets)
+
+# but NOTE: limited to most recent ~3000 tweets in the past few days!
+tweets <- searchTwitter("#APSA2014")
+tweets <- searchTwitter("#PoliSciNSF")
+tweets <- twListToDF(tweets)
+tweets$created
+
+#############################################
+### DOWNLOADING RECENT TWEETS FROM A USER ###
+#############################################
+
+## Here's how you can capture the most recent tweets (up to 3,200)
+## of any given user (in this case, @nytimes)
+
+## you can do this with twitteR
+timeline <- userTimeline('nytimes', n=20)
+timeline <- twListToDF(timeline)
+
+## but I have written my own function so that I can store the raw JSON data
+source("functions.r")
+
+getTimeline(filename="tweets_nytimes.json", screen_name="nytimes", 
+    n=3000, oauth=my_oauth, trim_user="false")
+
+# it's stored in disk and I can read it with the 'parseTweets' function in
+# the streamR package
+library(streamR)
+tweets <- parseTweets("tweets_nytimes.json")
+
+# see again smappR package (https://github.com/SMAPPNYU/smappR) for more
+
+###############################################
+### COLLECTING TWEETS FILTERING BY KEYWORDS ###
+###############################################
+
+library(streamR)
+
+filterStream(file.name="obama_tweets.json", track="obama", 
+    timeout=60, oauth=my_oauth)
+
+## Note the options:
+## FILE.NAME indicates the file in your disk where the tweets will be downloaded
+## TRACK is the keyword(s) mentioned in the tweets we want to capture.
+## TIMEOUT is the number of seconds that the connection will remain open
+## OAUTH is the OAuth token we are using
+
+## Once it has finished, we can open it in R as a data frame with the
+## "parseTweets" function
+tweets <- parseTweets("obama_tweets.json")
+
+## This is how we would capture tweets mentioning multiple keywords:
+filterStream(file.name="political_tweets.json", 
+	track=c("obama", "bush", "clinton"),
+    tweets=100, oauth=my_oauth)
+
+###############################################
+### COLLECTING TWEETS FILTERING BY LOCATION ###
+###############################################
+
+## This second example shows how to collect tweets filtering by location
+## instead. In other words, we can set a geographical box and collect
+## only the tweets that are coming from that area.
+
+## For example, imagine we want to collect tweets from the Arabian Peninsula.
+## The way to do it is to find two pairs of coordinates (longitude and latitude)
+## that indicate the southwest corner AND the northeast corner.
+## (NOTE THE REVERSE ORDER, IT'S NOT LAT, LONG BUT LONG, LAT)
+## In the case of the US, it would be approx. (-125,25) and (-66,50)
+## How to find the coordinates? I use: http://itouchmap.com/latlong.html
+
+filterStream(file.name="tweets_geo.json", locations=c(-125, 25, -66, 50), 
+    timeout=60, oauth=my_oauth)
+
+## Note that now we choose a different option, "TIMEOUT", which indicates for
+## how many seconds we're going to keep open the connection to Twitter.
+
+## But we could have chosen also tweets=100 instead
+
+## We can do as before and open the tweets in R
+tweets <- parseTweets("tweets_geo.json")
+
+############################################
+### COLLECTING A RANDOM SAMPLE OF TWEETS ###
+############################################
+
+## It's also possible to collect a random sample of tweets. That's what
+## the "sampleStream" function does:
+
+sampleStream(file.name="tweets_random.json", timeout=30, oauth=my_oauth)
+
+## Here I'm collecting 30 seconds of tweets
+## And once again, to open the tweets in R...
+tweets <- parseTweets("tweets_random.json")
+
+## What are the most common hashtags right now?
+getCommonHashtags(tweets$text)
+
+## What is the most retweeted tweet?
+tweets[which.max(tweets$retweet_count),]
 
 
 
--- functions.r
+++ functions.r
@@ -1,3 +1,196 @@
+################################################################################
+# Extract hashtags from a character vector and return frequency table
+################################################################################
+
+getCommonHashtags <- function(text, n=20){
+    hashtags <- regmatches(text,gregexpr("#(\\d|\\w)+",text))
+    hashtags <- unlist(hashtags)
+    tab <- table(hashtags)
+    return(head(sort(tab, dec=TRUE), n=n))
+}
+
+
+################################################################################
+# Download tweets sent by a given user
+################################################################################
+
+#' @rdname getTimeline
+#' @export
+#'
+#' @title 
+#' Returns up to 3,200 recent tweets from a given user
+#'
+#' @description
+#' \code{getTimeline} connects to the REST API of Twitter and returns up to
+#' 3,200 recent tweets sent by these user.
+#'
+#' @author
+#' Pablo Barbera \email{pablo.barbera@@nyu.edu}
+#'
+#' @param filename file where tweets will be stored (in json format)
+#'
+#' @param n number of tweets to be downloaded (maximum is 3,200)
+#'
+#' @param screen_name user name of the Twitter user for which his/her tweets
+#' will be downloaded
+#' 
+#' @param id id of Twitter user for which his/her tweets will be downloaded
+#' (Use either of these two arguments)
+#'
+#' @param oauth OAuth token
+#'
+#' @param since_id id of the oldest tweet to be downloaded. Useful if, for 
+#' example, we're only interested in getting tweets sent after a certain
+#' date.
+#'
+#' @param trim_user if "true", downloaded tweets will include user object
+#' embedded. If "false", only tweet information will be downloaded.
+#'
+#' @param sleep numeric, number of seconds between API calls. Higher number
+#' will increase reliability of API calls; lower number will increase speed.
+#'
+#' @examples \dontrun{
+#' ## Download recent tweets by user "p_barbera"
+#'  friends <- getTimeline(screen_name="p_barbera", oauth=my_oauth)
+#' }
+#'
+
+getTimeline <- function(filename, n=3200, oauth, screen_name=NULL, 
+    id=NULL, since_id=NULL, trim_user="true", sleep=.5){
+
+    require(rjson); require(ROAuth)
+
+    ## while rate limit is 0, open a new one
+    limit <- getLimitTimeline(my_oauth)
+    cat(limit, " hits left\n")
+    while (limit==0){
+        Sys.sleep(sleep)
+        # sleep for 5 minutes if limit rate is less than 100
+        rate.limit <- getLimitRate(my_oauth)
+        if (rate.limit<100){
+            Sys.sleep(300)
+        }
+        limit <- getLimitTimeline(my_oauth)
+        cat(limit, " hits left\n")
+    }
+    ## url to call
+    url <- "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+    ## first API call
+    if (!is.null(screen_name)){
+        params <- list(screen_name = screen_name, count=200, trim_user=trim_user)
+    }
+    if (!is.null(id)){
+        params <- list(id=id, count=200, trim_user=trim_user)   
+    }
+    if (!is.null(since_id)){
+        params[["since_id"]] <- since_id
+    }
+    
+    url.data <- my_oauth$OAuthRequest(URL=url, params=params, method="GET", 
+    cainfo=system.file("CurlSSL", "cacert.pem", package = "RCurl")) 
+    Sys.sleep(sleep)
+    ## one API call less
+    limit <- limit - 1
+    ## changing oauth token if we hit the limit
+    cat(limit, " hits left\n")
+    while (limit==0){
+        Sys.sleep(sleep)
+        # sleep for 5 minutes if limit rate is less than 100
+        rate.limit <- getLimitRate(my_oauth)
+        if (rate.limit<100){
+            Sys.sleep(300)
+        }
+        limit <- getLimitTimeline(my_oauth)
+        cat(limit, " hits left\n")
+    }
+    ## trying to parse JSON data
+    ## json.data <- fromJSON(url.data, unexpected.escape = "skip")
+    json.data <- RJSONIO::fromJSON(url.data)
+    if (length(json.data$error)!=0){
+        cat(url.data)
+        stop("error! Last cursor: ", cursor)
+    }
+    ## writing to disk
+    conn <- file(filename, "a")
+    invisible(lapply(json.data, function(x) writeLines(rjson::toJSON(x), con=conn)))
+    close(conn)
+    ## max_id
+    tweets <- length(json.data)
+    max_id <- json.data[[tweets]]$id_str
+    cat(tweets, "tweets. Max id: ", max_id, "\n")
+    max_id_old <- "none"
+    if (is.null(since_id)) {since_id <- 1}
+
+    while (tweets < n & max_id != max_id_old & 
+        as.numeric(max_id) > as.numeric(since_id)){
+        max_id_old <- max_id
+        if (!is.null(screen_name)){
+            params <- list(screen_name = screen_name, count=200, max_id=max_id,
+                trim_user=trim_user)
+        }
+        if (!is.null(id)){
+            params <- list(id=id, count=200, max_id=max_id, trim_user=trim_user)
+        }
+        if (!is.null(since_id)){
+            params[['since_id']] <- since_id
+        }
+        url.data <- my_oauth$OAuthRequest(URL=url, params=params, method="GET", 
+        cainfo=system.file("CurlSSL", "cacert.pem", package = "RCurl")) 
+        Sys.sleep(sleep)
+        ## one API call less
+        limit <- limit - 1
+        ## changing oauth token if we hit the limit
+        cat(limit, " hits left\n")
+        while (limit==0){
+            Sys.sleep(sleep)
+            # sleep for 5 minutes if limit rate is less than 100
+            rate.limit <- getLimitRate(my_oauth)
+            if (rate.limit<100){
+                Sys.sleep(300)
+            }
+            limit <- getLimitTimeline(my_oauth)
+            cat(limit, " hits left\n")
+        }
+        ## trying to parse JSON data
+        ## json.data <- fromJSON(url.data, unexpected.escape = "skip")
+        json.data <- RJSONIO::fromJSON(url.data)
+        if (length(json.data$error)!=0){
+            cat(url.data)
+            stop("error! Last cursor: ", cursor)
+        }
+        ## writing to disk
+        conn <- file(filename, "a")
+        invisible(lapply(json.data, function(x) writeLines(rjson::toJSON(x), con=conn)))
+        close(conn)
+        ## max_id
+        tweets <- tweets + length(json.data)
+        max_id <- json.data[[length(json.data)]]$id_str
+        cat(tweets, "tweets. Max id: ", max_id, "\n")
+    }
+}
+
+
+getLimitTimeline <- function(my_oauth){
+    require(rjson); require(ROAuth)
+    url <- "https://api.twitter.com/1.1/application/rate_limit_status.json"
+    params <- list(resources = "statuses,application")
+    response <- my_oauth$OAuthRequest(URL=url, params=params, method="GET", 
+        cainfo=system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+    return(unlist(fromJSON(response)$resources$statuses$`/statuses/user_timeline`[['remaining']]))
+
+}
+
+
+getLimitRate <- function(my_oauth){
+    require(rjson); require(ROAuth)
+    url <- "https://api.twitter.com/1.1/application/rate_limit_status.json"
+    params <- list(resources = "followers,application")
+    response <- my_oauth$OAuthRequest(URL=url, params=params, method="GET", 
+        cainfo=system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+    return(unlist(fromJSON(response)$resources$application$`/application/rate_limit_status`[['remaining']]))
+}
+
 
 ################################################################################
 # Scrape NYT Congress API for information on Members of 114th Congress
@@ -42,14 +235,3 @@ unlistWithNA <- function(lst, field){
     return(vect)
 }
 
-
-################################################################################
-# Extract hashtags from a character vector and return frequency table
-################################################################################
-
-getCommonHashtags <- function(text, n=20){
-    hashtags <- regmatches(text,gregexpr("#(\\d|\\w)+",text))
-    hashtags <- unlist(hashtags)
-    tab <- table(hashtags)
-    return(head(sort(tab, dec=TRUE), n=n))
-}
